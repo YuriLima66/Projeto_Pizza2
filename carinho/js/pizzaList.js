453d4ec31d6d25c34ed13594009e36e991d0c2d6
0osvaldo Djata <djata.osvaldo@gmail.com>
2024-08-14 15:48:25 -0300

    projeto feito por mim para uma pizzaria

---
+++ carinho/js/pizzaList.js
@@ -0,0 +1,109 @@
+let PizzaList = [
+  {
+    id: 1,
+    name: "Mussarela",
+    img: "images/pizza.png",
+    prices: [10.0, 15.0, 30.19],
+    sizes: ["100g", "530g", "860g"],
+    description:"Pizza tradicional Mussarela, uma ótima pedida. 🤤",
+  },
+  {
+    id: 2,
+    name: "Calabresa",
+    img: "images/pizza2.png",
+    prices: [10.0, 15.0, 20.0],
+    sizes: ["320g", "530g", "860g"],
+    description: "Pizza calabresa tradicional topissima. 😋",
+  },
+  {
+    id: 3,
+    name: "Quatro Queijos",
+    img: "images/pizza3.png",
+    prices: [12.0, 18.0, 25.45],
+    sizes: ["320g", "530g", "860g"],
+    description:"Se é queijo que você quer 🤩, então essa pizza é para você!",
+  },
+  {
+    id: 4,
+    name: "Americana",
+    img: "images/pizza4.png",
+    prices: [13.0, 17.0, 28.77],
+    sizes: ["320g", "530g", "860g"],
+    description:"Pizza americana tradicional.",
+  },
+  {
+    id: 5,
+    name: "Portuguesa",
+    img: "images/pizza5.png",
+    prices: [10.0, 17.0, 28.43],
+    sizes: ["320g", "530g", "860g"],
+    description:"Pizza portuguesa, uma dellícia. 🤩",
+  },
+
+  {
+    id: 6,
+    name: "Refrigerante",
+    img: "images/coca1.png",
+    prices: [5.10, 7.40, 12.0],
+    sizes: ["250ml", "1.5L", "2L"],
+    description:"Refrigerante coca cola",
+  },
+
+  
+  {
+    id: 7,
+    name: "M&Ms",
+    img: "images/pizza7.png",
+    prices: [10.0, 15.0, 22.36],
+    sizes: ["320g", "530g", "860g"],
+    description:"Pizza doce de M&Ms e nutella, perfeito para fechar com chave de ouro.",
+  },
+
+
+  {
+    id: 8,
+    name: "Heineken",
+    img: "images/heinehen.png",
+    prices: [5.90, 7.50, 15.80],
+    sizes: ["269ml", "350ml", "600ml"],
+    description:"Cerveja Premiun",
+  },
+  {
+
+    
+    id: 9,
+    name: "Marguerita",
+    img: "images/pizza8.png",
+    prices: [13.0, 19.0, 30.43],
+    sizes: ["320g", "530g", "860g"],
+    description:"Pizza Margerita, dellícia. 🤩",
+  },
+
+  {
+    id: 10,
+    name: "patagonia",
+    img: "images/patagonia.jpg",
+    prices: [4.90, 7.90, 17.80],
+    sizes: ["269ml", "350ml", "600ml"],
+    description:"Cerveja Premiun",
+  },
+
+  {
+    id: 11,
+    name: "guarana",
+    img: "images/guarana.png",
+    prices: [2.90, 5.90, 9.80],
+    sizes: ["350ml", "1L", "2L"],
+    description:"Refrigerante Guarana",
+  },
+
+  {
+    id: 12,
+    name: "Agua",
+    img: "images/Agua.jpg",
+    prices: [2.90, 3.90, 4.80],
+    sizes: ["350ml", "1L", "1.5L"],
+    description:"Agua Mineral",
+  },
+  
+];
